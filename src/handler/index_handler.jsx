b350ad45ac56663fdcfd789d09cce15ecaf38a66
Eugene Matvejevs <eugene.matvejev@gmail.com>
2017-03-19 16:28:57 +0000

    imporve index handler

--- src/handler/index_handler.jsx
+++ src/handler/index_handler.jsx
@@ -1,23 +1,37 @@
 import React from "react";
-import Battlefield from "../component/battlefield";
-import Cell from "../component/cell";
-import Player from "../component/player";
-import PlayerModel from '../model/player_model';
-import BattlefieldGenerator from "../generator/battlefield_generator";
-import GameGenerator from "../generator/game_generator";
+import Game from "../component/game";
+import generator from "../generator/game_generator";
+import Slider from "react-rangeslider";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-rangeslider/lib/index.css";
+import "../stylesheets/css/overwritten.css";
 
 export default class Handler extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            opponents: 1,
+            size: 10
+        }
+    }
+
     render() {
-        const gameModel = GameGenerator.generate(2, 10);
-        const battlefieldModel = BattlefieldGenerator.generate(10);
-        const playerModel = new PlayerModel();
-        playerModel.setEmail('example@example.com');
-        playerModel.setUsername('example-username');
+        const opponents = this.state.opponents;
+        const size = this.state.size;
+
+        const gameModel = generator.generate((opponents + 1), size);
 
         return (
             <div>
-                <Player model={playerModel}/>
-                <Battlefield model={battlefieldModel}/>
+                <fieldset className="col-md-6">
+                    <div>opponents: {opponents}</div>
+                    <Slider min={1} max={3} step={1} value={opponents} onChange={(val) => this.setState({ opponents: val })}/>
+                    <div>game size: {size}</div>
+                    <Slider min={5} max={15} step={1} value={size} onChange={(val) => this.setState({ size: val })}/>
+                </fieldset>
+
+                <Game className="col-md-12" model={gameModel}/>
             </div>
         );
     }
