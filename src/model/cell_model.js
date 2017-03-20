85d2eed25e91fab55c2d74024402497d886ba254
Eugene Matvejevs <eugene.matvejev@gmail.com>
2017-03-20 07:21:45 +0000

    cell flags

--- src/model/cell_model.js
+++ src/model/cell_model.js
@@ -1,4 +1,10 @@
-import BytesAwareModel from './abstract_bytes_aware_model'
+import BytesAwareModel from "./abstract_bytes_aware_model";
+
+const FLAG_NONE = 0x00;
+const FLAG_DEAD = 0x01;
+const FLAG_SHIP = 0x02;
+const FLAG_SKIP = 0x04;
+/** const FLAG_DEAD_SHIP = FLAG_SHIP | FLAG_DEAD; */
 
 export default class CellModel extends BytesAwareModel {
     constructor(coordinate) {
@@ -17,5 +23,12 @@ export default class CellModel extends BytesAwareModel {
 
     getCoordinate() {
         return this.coordinate;
+    }
+
+    static flags = {
+        none: FLAG_NONE,
+        dead: FLAG_DEAD,
+        ship: FLAG_SHIP,
+        skip: FLAG_SKIP
     }
 }
