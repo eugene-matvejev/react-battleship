1bf157949ef865be7a94e543d589d1b29442640b
Eugene Matvejevs <eugene.matvejev@gmail.com>
2017-03-20 07:22:49 +0000

    add 'mock' data

--- src/generator/game_generator.js
+++ src/generator/game_generator.js
@@ -1,25 +1,53 @@
 import GameModel from "../model/game_model";
 import BattlefieldGenerator from "../generator/battlefield_generator";
-import PlayerModel from '../model/player_model';
+import CellModel from "../model/cell_model";
+import PlayerModel from "../model/player_model";
 
 export default class GameGenerator {
     /**
-     * @param {Number} number
+     * @param {Number} players
      * @param {Number} size
      *
      * @returns {GameModel}
      */
-    static generate(number, size) {
+    static generate(players, size) {
         const model = new GameModel();
 
-        for (let i = 0; i < number; i++) {
+        for (let i = 0; i < players; i++) {
             const battlefield = BattlefieldGenerator.generate(size);
             const player = new PlayerModel();
-                battlefield.setPlayer(player);
+            battlefield.setPlayer(player);
 
             model.addBattlefield(battlefield)
         }
 
+        GameGenerator.applyPlayerShips(model);
+
         return model;
+    }
+
+    /**
+     * @param {GameModel} model
+     */
+    static applyPlayerShips(model) {
+        let cells = ['A1', 'A2', 'A3', 'A5', 'C1', 'C2', 'C5'];
+
+        for (const battlefield of model.getBattlefields()) {
+            if (battlefield.getPlayer().hasBytes(0x01)) {
+                continue;
+            }
+
+            cells.forEach(coordinate => battlefield.getCell(coordinate).setBytes(CellModel.flags.ship));
+        }
+
+        cells = ['B1', 'B2'];
+
+        for (const battlefield of model.getBattlefields()) {
+            if (battlefield.getPlayer().hasBytes(0x01)) {
+                continue;
+            }
+
+            cells.forEach(coordinate => battlefield.getCell(coordinate).setBytes(CellModel.flags.dead));
+        }
     }
 }
