0a1136adc6c53d7f15907a1bfc148edcfa0468ef
Eugene Matvejev <jevgenijs.matvejevs@wipro.com>
2017-09-01 16:44:12 +0100

    push unfinished changes for enzyme

--- src/handler/index_handler.test.jsx
+++ src/handler/index_handler.test.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import IndexHandler from "./index_handler";
+import {shallow} from "enzyme";
 
 describe('handler:: Index', () => {
     describe(`::render`, () => {
@@ -9,5 +10,13 @@ describe('handler:: Index', () => {
 
             ReactDOM.render(<IndexHandler size={1} opponents={1}/>, div);
         });
+    });
+
+    describe(`::slider`, () => {
+        const el = shallow(<IndexHandler size={1} opponents={1}/>);
+
+        el.find('Slider[min=1]').simulate('change', 2);
+
+        expect(el.find('.battlefield-cells').children()).toBe(3);
     });
 });
