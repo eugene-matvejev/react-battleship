a18e4c495b8738c22d54238db0c6d5e7b7984055
Eugene Matvejev <jevgenijs.matvejevs@wipro.com>
2017-09-04 16:55:09 +0100

    additional test on slider

--- src/handler/index_handler.test.jsx
+++ src/handler/index_handler.test.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import IndexHandler from "./index_handler";
 import {shallow} from "enzyme";
+import parameters from "../parameters.json";
 
 describe(`handler:: <IndexHandler/>`, () => {
     describe(`::render`, () => {
@@ -19,6 +20,17 @@ describe(`handler:: <IndexHandler/>`, () => {
             el.find('Slider[min=1]').simulate('change', 2);
 
             expect(el.find('Game').prop('model').battlefields.length).toBe(3);
+        });
+
+        it(` - should generate & render additional cells as game size changed from 1 to 2`, () => {
+            const el = shallow(<IndexHandler size={1} opponents={1}/>);
+
+            el.find(`Slider[min=${parameters.minGameSize}]`).simulate('change', parameters.maxGameSize);
+
+            const expected = parameters.maxGameSize ** 2;
+            const cells = el.find('Game').prop('model').battlefields[0].cells;
+
+            expect(Object.keys(cells).length).toBe(expected);
         });
     });
 });
