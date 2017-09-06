5852c4ba240e5d67ff321bbb06abc0944f79594f
Eugene Matvejev <jevgenijs.matvejevs@wipro.com>
2017-09-06 17:29:28 +0100

    fix navigation handler test

--- src/handler/navigation_handler.test.jsx
+++ src/handler/navigation_handler.test.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import NavigationHandler from "./navigation_handler";
+import {BrowserRouter} from "react-router-dom";
 
 describe(`handler:: <NavigationHandler/>`, () => {
     describe(`::render`, () => {
         it(`- renders without crash`, () => {
             const div = document.createElement('div');
 
-            ReactDOM.render(<NavigationHandler/>, div);
+            ReactDOM.render(
+                <BrowserRouter>
+                    <NavigationHandler/>
+                </BrowserRouter>,
+                div
+            );
         })
     })
 });
