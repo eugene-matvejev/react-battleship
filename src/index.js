b9a32777347a8bbf538151778ba30cbb0758a517
Eugene Matvejev <eugene.matvejev@gmail.com>
2017-09-07 10:52:45 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppKernel from "./app/app_kernel";
-// import IndexHandler from "./handler/navigation_handler";
 
 ReactDOM.render(<AppKernel/>, document.getElementById('content-area'));
-// ReactDOM.render(<IndexHandler/>, document.getElementById('navigation-area'));
