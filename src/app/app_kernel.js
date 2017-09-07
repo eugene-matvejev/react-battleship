06323412e7b8f4e9333da83f8320042e84a37238
Eugene Matvejev <eugene.matvejev@gmail.com>
2017-09-07 10:57:27 +0100

    Update app_kernel.js

--- src/app/app_kernel.js
+++ src/app/app_kernel.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
-import IndexHandler from "../handler/navigation_handler";
+import NavigationHandler from "../handler/navigation_handler";
 import GameHandler from "../handler/game_handler";
 import GameResultsHandler from "../handler/game_results_handler";
 import "../stylesheets/css/overwritten.css";
@@ -8,8 +8,8 @@ import "../stylesheets/css/overwritten.css";
 const appKernel = () =>
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={(props) => <IndexHandler {...props}/>}/>
-            <Route exact path="/index" component={(props) => <IndexHandler {...props}/>}/>
+            <Route exact path="/" component={(props) => <NavigationHandler {...props}/>}/>
+            <Route exact path="/index" component={(props) => <NavigationHandler {...props}/>}/>
 
             <Route exact path="/game-new" component={(props) => <GameHandler {...props}/>}/>
             <Route exact path="/game-current" component={(props) => <GameHandler {...props}/>}/>
