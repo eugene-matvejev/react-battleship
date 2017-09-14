78c5e88bfb658c7d6fee1e6dcc9de0b819bf7b6a
Eugene Matvejev <jevgenijs.matvejevs@wipro.com>
2017-09-14 17:35:57 +0100

    improve game result handler

--- src/handler/game_results_handler.test.jsx
+++ src/handler/game_results_handler.test.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import GameResultsHandler from "./game_results_handler";
+import {mount} from "enzyme";
 
 describe(`handler:: <GameResultsHandler/>`, () => {
     describe(`::render`, () => {
@@ -9,5 +10,15 @@ describe(`handler:: <GameResultsHandler/>`, () => {
 
             ReactDOM.render(<GameResultsHandler/>, div);
         });
-    })
+    });
+
+    describe(`::paginationOnClickCallback`, () => {
+        it(` - callback should modify state [currentPage]`, () => {
+            const component = mount(<GameResultsHandler currrentPage={1} totalPages={2}/>);
+
+            component.find('.next').simulate('click');
+
+            expect(component.state().currentPage).toBe(2);
+        });
+    });
 });
