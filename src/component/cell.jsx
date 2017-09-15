998ae67f177f4cb6692726154f45402eccce22ce
Eugene Matvejev <jevgenijs.matvejevs@wipro.com>
2017-09-15 15:43:48 +0100

    convert <Cell/> component to stateless

--- src/component/cell.jsx
+++ src/component/cell.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-import AbstractComponent from "./abstract_component";
 import CellModel from "../model/cell_model";
 import "../stylesheets/css/cell.css";
 
-export default class Cell extends AbstractComponent {
-    render() {
-        const attributes = {
-            'data-id': '<cell-id>',
-            'data-bytes': this.props.model.getSequence(),
-            'data-coordinate': this.props.model.getCoordinate(),
-        };
+const Cell = (props) => {
+    const attributes = {
+        'data-id': '<cell-id>',
+        'data-bytes': props.model.getSequence(),
+        'data-coordinate': props.model.getCoordinate(),
+    };
 
-        return (
-            <div className={`component battlefield-cell ${this.props.className || ''}`} {...attributes}>
-                {this.props.model.getCoordinate()}
-            </div>
-        );
-    }
+    return (
+        <div className={`component battlefield-cell ${this.props.className || ''}`} {...attributes}>
+            {this.props.model.getCoordinate()}
+        </div>
+    );
+};
 
-    static propTypes = {
-        model: PropTypes.instanceOf(CellModel).isRequired,
-        className: PropTypes.string
-    }
-}
+Cell.propTypes = {
+    model: PropTypes.instanceOf(CellModel).isRequired,
+    className: PropTypes.string
+};
+
+export default Cell;
