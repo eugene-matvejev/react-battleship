18f3ddbc0adea0f9c8c1b573e754d4ca3d89c76c
Eugene Matvejev <jevgenijs.matvejevs@wipro.com>
2017-09-15 14:38:19 +0100

    save progress

--- src/app/app_kernel.js
+++ src/app/app_kernel.js
@@ -2,7 +2,7 @@ import React from "react";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import NavigationHandler from "../handler/navigation_handler";
 import GameHandler from "../handler/game_handler";
-// import GameResultsHandler from "../handler/game_results_handler";
+import GameResultsHandler from "../handler/game_results_handler";
 import "../stylesheets/css/overwritten.css";
 
 const appKernel = () =>
@@ -13,7 +13,7 @@ const appKernel = () =>
 
             <Route exact path="/game-new" component={GameHandler}/>
             <Route exact path="/game-current" component={GameHandler}/>
-            <Route exact path="/game-results" component={GameHandler}/>
+            <Route exact path="/game-results" component={GameResultsHandler}/>
         </Switch>
     </BrowserRouter>;
 
