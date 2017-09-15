87843bc3ef7b0e1055d277f9fc98e37dc0d1dec6
Eugene Matvejev <eugene.matvejev@gmail.com>
2017-09-15 11:12:46 +0100

    Update app_kernel.js

--- src/app/app_kernel.js
+++ src/app/app_kernel.js
@@ -6,15 +6,25 @@ import GameHandler from "../handler/game_handler";
 import "../stylesheets/css/overwritten.css";
 
 const appKernel = () =>
+//     <BrowserRouter>
+//         <Switch>
+//             <Route exact path="/" component={(props) => <NavigationHandler {...props}/>}/>
+//             <Route exact path="/index" component={(props) => <NavigationHandler {...props}/>}/>
+
+//             <Route exact path="/game-new" component={(props) => <GameHandler {...props}/>}/>
+//             <Route exact path="/game-current" component={(props) => <GameHandler {...props}/>}/>
+//             <Route exact path="/game-results" component={(props) => <GameHandler {...props}/>}/>
+//         </Switch>
+//     </BrowserRouter>;
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={(props) => <NavigationHandler {...props}/>}/>
-            <Route exact path="/index" component={(props) => <NavigationHandler {...props}/>}/>
+            <Route exact path="/" component={NavigationHandler}/>
+            <Route exact path="/index" component={NavigationHandler}/>
 
-            <Route exact path="/game-new" component={(props) => <GameHandler {...props}/>}/>
-            <Route exact path="/game-current" component={(props) => <GameHandler {...props}/>}/>
-//             <Route exact path="/game-results" component={(props) => <GameResultsHandler {...props}/>}/>
-            <Route exact path="/game-results" component={(props) => <GameHandler {...props}/>}/>
+            <Route exact path="/game-new" component={GameHandler}/>
+            <Route exact path="/game-current" component={GameHandler}/>
+//             <Route exact path="/game-results" component={GameResultsHandler}/>
+            <Route exact path="/game-results" component={GameHandler}/>
         </Switch>
     </BrowserRouter>;
 
