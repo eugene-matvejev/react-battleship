1306b490bcdc84a0d910d0e98582356c9c1f9cc9
Eugene Matvejev <eugene.matvejev@gmail.com>
2017-09-15 15:45:39 +0100

    Update player.jsx

--- src/component/player.jsx
+++ src/component/player.jsx
@@ -4,21 +4,19 @@ import PlayerModel from "../model/player_model";
 import "../stylesheets/css/player.css";
 
 const Player = (props) => {
-    /** @param {PlayerModel} model */
     const model = props.model;
-
     const attributes = {
         'data-player-id': model.getId(),
         'data-player-flag': model.getSequence(),
     };
 
     return (
-        <div className={`component battlefield-player ${this.props.className || ''}`} {...attributes}>
+        <div className={`component battlefield-player ${props.className || ''}`} {...attributes}>
             <div className="player-avatar">
                 <img src="/assets/img/avatar-placeholder.png" alt="avatar pic"/>
             </div>
-            <div className="player-name">{'< playerName >'}</div>
-            <div className="player-score">{'< playerScore >'}</div>
+            <div className="player-name">{`< playerName >`}</div>
+            <div className="player-score">{`< playerScore >`}</div>
         </div>
     );
 };
