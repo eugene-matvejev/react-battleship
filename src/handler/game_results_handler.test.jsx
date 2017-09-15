8afd10de35b5c84187b5ad0518bf2bea6f9a678c
Eugene Matvejev <jevgenijs.matvejevs@wipro.com>
2017-09-15 15:16:25 +0100

    more and better test cases for <GameResultsHandler/>, for https://github.com/airbnb/enzyme/issues/441

--- src/handler/game_results_handler.test.jsx
+++ src/handler/game_results_handler.test.jsx
@@ -13,20 +13,35 @@ describe(`handler:: <GameResultsHandler/>`, () => {
     });
 
     describe(`::paginationOnClickCallback`, () => {
-        it(` - callback should modify state [currentPage]`, () => {
-            const component = mount(<GameResultsHandler currrentPage={1} totalPages={2}/>);
+        describe(` - callback should modify state [currentPage]`, () => {
+            [
+                {current: 1, total: 2, selector: '.next', expected: 2},
+                {current: 2, total: 2, selector: '.prev', expected: 1},
+            ].forEach((el) => {
+                it(` - current: ${el.current}, total: ${el.total}, on: "${el.selector}", expected: ${el.expected}`, () => {
+                    const component = mount(<GameResultsHandler currentPage={el.current} totalPages={el.total}/>);
 
-            component.find('.next').simulate('click');
+                    component.find(el.selector).simulate('click');
 
-            expect(component.state().currentPage).toBe(2);
+                    expect(component.state().currentPage).toBe(el.expected);
+                });
+            });
         });
+    });
 
-        it(` - callback should modify state [currentPage]`, () => {
-            const component = mount(<GameResultsHandler currrentPage={1} totalPages={2}/>);
+    describe(`::keyDownEventHandler`, () => {
+        [
+            {current: 1, total: 2, code: 'ArrowRight', expected: 2},
+            {current: 2, total: 2, code: 'ArrowLeft', expected: 1},
+            {current: 1, total: 2, code: 'unknown', expected: 1},
+        ].forEach((el) => {
+            it(` - current: ${el.current}, total: ${el.total}, on: "${el.code}", expected: ${el.expected}`, () => {
+                const component = mount(<GameResultsHandler currentPage={el.current} totalPages={el.total}/>);
 
-            component.simulate('keydown', {code: 'ArrowRight'});
+                component.simulate('keydown', {code: el.code});
 
-            expect(component.state().currentPage).toBe(2);
+                expect(component.state().currentPage).toBe(el.expected);
+            });
         });
     });
 });
