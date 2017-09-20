08eb32dbb3fc5fe68816ade8a91a5f0787edfd51
Eugene Matvejev <eugene.matvejev@gmail.com>
2017-09-20 15:27:26 +0100

    Update game_generator.test.js

--- src/service/generator/game_generator.test.js
+++ src/service/generator/game_generator.test.js
@@ -1,26 +1,41 @@
 import GameGenerator from "./game_generator";
+import PlayerModel from "../../model/player_model";
 
 describe(`generator:: <GameGenerator/>`, () => {
     describe(`::generate`, () => {
-        [0, 1, 2, 10].forEach(number => {
-            it(` - expected ${number} battlefields attached`, () => {
-                const model = GameGenerator.generate(number, 1);
+        [0, 1, 2, 10].forEach(v => {
+            it(` - expected ${v} battlefields`, () => {
+                const model = GameGenerator.generate(v, 0);
 
-                expect(model.getBattlefields().length).toBe(number);
+                expect(model.getBattlefields().length).toBe(v);
             });
         });
 
-        [1].forEach(size => {
-            it(` - expected 2 battlefields with size ${size} attached`, () => {
+        [1, 2, 10].forEach(v => {
+            it(` - expected last player of ${v} to be market as Human Controlled`, () => {
+                const model = GameGenerator.generate(v, 0);
+
+                model.getBattlefields().forEach((bf, index) => {
+                    const expectedSeq = index === (v - 1) ? PlayerModel.getHumanFlag() : 0;
+
+                    expect(bf.getPlayer().byte_sequence).toBe(expectedSeq);
+                });
+            });
+        });
+
+        [0, 1, 2, 10].forEach(size => {
+            const expectedCellAmount = size ** 2;
+
+            it(` - expected 2 battlefields with size ${size} attached [expected ${expectedCellAmount} cells]`, () => {
                 const model = GameGenerator.generate(2, size);
 
                 /** @param {BattlefieldModel} battlefieldModel */
                 for (const battlefieldModel of model.getBattlefields()) {
-                    const cells = battlefieldModel.getCells();
+                    const cells = battlefieldModel.getCellsIndexedByCoordinate();
 
-                    expect(Object.keys(cells).length).toBe(size ** 2);
+                    expect(Object.keys(cells).length).toBe(expectedCellAmount);
                 }
             });
         });
-    })
+    });
 });
