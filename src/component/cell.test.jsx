43353d5cf05d04c56192eebb2fe4bfb882f407a0
Eugene Matvejev <eugene.matvejev@gmail.com>
2017-12-06 16:48:35 +0000

    Update cell.test.jsx

--- src/component/cell.test.jsx
+++ src/component/cell.test.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import {shallow} from "enzyme";
-import Cell from "./cell";
-import CellModel from "../model/cell_model";
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Cell } from './';
+import { CellModel } from '../model';
 
 describe(`component:: <Cell/>`, () => {
     describe(`::render`, () => {
         it(' - renders without error', () => {
             const model = new CellModel('A1');
 
-            shallow(<Cell model={model}/>);
+            shallow(<Cell model={model} />);
         });
     });
 });
