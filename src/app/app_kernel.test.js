757cd1aa86b9931c84d8bc9a5163afc17bd01a33
Eugene Matvejev <eugene.matvejev@gmail.com>
2017-12-06 16:44:51 +0000

    port <AppKernel/> tests

--- src/app/app_kernel.test.js
+++ src/app/app_kernel.test.js
@@ -1,11 +1,11 @@
-import React from "react";
-import {shallow} from "enzyme"
-import AppKernel from "./app_kernel";
+import React from 'react';
+import { shallow } from 'enzyme';
+import AppKernel from './app_kernel';
 
 describe(`component:: <AppKernel/>`, () => {
     describe('::render', () => {
-        it(`renders without crash`, () => {
-            shallow(<AppKernel/>);
+        it(' - without error', () => {
+            shallow(<AppKernel />);
         });
     });
 });
