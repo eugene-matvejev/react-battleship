717c99ad097ad63b54d1c1b8973fc80b24fe2707
Eugene Matvejev <eugene.matvejev@gmail.com>
2017-12-22 14:47:40 +0000

    Update game_generator.test.js

--- src/service/generator/game_generator.test.js
+++ src/service/generator/game_generator.test.js
@@ -1,11 +1,11 @@
-import GameGenerator from "./game_generator";
+import { generateGame } from "./";
 import PlayerModel from "../../model/player_model";
 
-describe(`generator:: <GameGenerator/>`, () => {
+describe(`generator:: Game`, () => {
     describe(`::generate`, () => {
-        [0, 1, 2, 10].forEach(v => {
+        [0, 1, 2, 10].forEach((v) => {
             it(`expected ${v} battlefields`, () => {
-                const model = GameGenerator.generate(v, 0);
+                const model = generateGame(v, 0);
 
                 expect(model.getBattlefields().length).toBe(v);
             });
@@ -13,12 +13,12 @@ describe(`generator:: <GameGenerator/>`, () => {
 
         [1, 2, 10].forEach(v => {
             it(`expected last player of ${v} to be market as Human Controlled`, () => {
-                const model = GameGenerator.generate(v, 0);
+                const model = generateGame(v, 0);
 
                 model.getBattlefields().forEach((bf, index) => {
                     const expectedSeq = index === (v - 1) ? PlayerModel.getHumanFlag() : 0;
 
-                    expect(bf.getPlayer().byte_sequence).toBe(expectedSeq);
+                    expect(bf.getPlayer().byteSeq).toBe(expectedSeq);
                 });
             });
         });
@@ -27,7 +27,7 @@ describe(`generator:: <GameGenerator/>`, () => {
             const expectedCellAmount = size ** 2;
 
             it(`expected 2 battlefields with size ${size} attached [expected ${expectedCellAmount} cells]`, () => {
-                const model = GameGenerator.generate(2, size);
+                const model = generateGame(2, size);
 
                 /** @param {BattlefieldModel} battlefieldModel */
                 for (const battlefieldModel of model.getBattlefields()) {
