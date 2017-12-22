d294b733153949719c5cd0f5338f18834df24038
Eugene Matvejev <jevgenijs.matvejevs@wipro.com>
2017-12-22 15:08:20 +0000

    fix build

--- src/component/game.test.jsx
+++ src/component/game.test.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {shallow} from "enzyme";
 import Game from "./game";
-import GameGenerator from "../service/generator/game_generator";
+import { generateGame } from "../service/generator";
 
 describe(`component:: <Game/>`, () => {
     describe('::render', () => {
         it('- renders without error', () => {
-            const model = GameGenerator.generate(1, 1);
+            const model = generateGame(1, 1);
 
             shallow(<Game model={model}/>);
         });
