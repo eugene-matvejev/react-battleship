c34ff872911264bfcbc5ec2f3135f5287023a3e1
Eugene Matvejev <eugene.matvejev@gmail.com>
2017-12-25 13:04:26 +0000

    Update game_initiation_handler.test.jsx

--- src/handler/game_initiation_handler.test.jsx
+++ src/handler/game_initiation_handler.test.jsx
@@ -30,11 +30,11 @@ describe(`handler:: <GameInitiationHandler/>`, () => {
             expect(component.state('opponents')).toBe(maxOpponents);
         });
 
-        it(`should generate & render additional cells as game size changed from ${minGameSize} to ${maxGameSize}`, () => {
+        it(`should generate & render additional cells as game size changed from ${minSize} to ${maxSize}`, () => {
             const component = shallow(<GameInitiationHandler {...defaultProps} />);
-            component.find(`Slider`).at(1).simulate('change', maxGameSize);
+            component.find(`Slider`).at(1).simulate('change', maxSize);
 
-            const expected = maxGameSize ** 2;
+            const expected = maxSize ** 2;
             const cells = component.find('Battlefield').prop('model').cellsIndexedByCoordinate;
 
             expect(Object.keys(cells).length).toBe(expected);
