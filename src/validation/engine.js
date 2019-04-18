7aaf977b95c00ef8e6886f6a70b32f319b1c3448
Eugene Matvejev <eugene.matvejev@gmail.com>
2019-04-18 23:02:00 +0100

    validation engine & its tests

--- src/validation/engine.js
+++ src/validation/engine.js
@@ -1,12 +1,65 @@
-export default (c) => {
+const executeValidations = (item, c) => {
+    const { validators, value } = item;
+
+    if (!Array.isArray(validators)) {
+        return true;
+    }
+
+    item.errors = [];
+
     let isValid = true;
-    for (const { items } of c) {
-        for (const { validators, value: v } of items) {
-            if (!Array.isArray(validators)) {
+
+    for (const fn of validators) {
+        const r = fn(value, c);
+        /**
+         * if result is a string, then it is considered as error, otherwise it considered as valid
+         */
+        if (typeof r === 'string') {
+            isValid = false;
+
+            item.errors.push(r);
+        }
+    }
+
+    return isValid;
+}
+
+/**
+ * @param {Array} c
+ * @param {Array} enforceValidationOn
+ *
+ * example of enforceValidationOn
+ *  [[sectionId, itemId]] - will validate only item in specific section
+ *  [[sectionId, undefined]] - will validate entire section, if item is not defined
+ */
+export const validationEngine = (c, enforceValidationOn) => {
+    let isValid = true;
+
+    if (Array.isArray(enforceValidationOn)) {
+        for (const [sectionId, itemId] of enforceValidationOn) {
+            if (undefined === itemId) {
+                for (const item of c[sectionId].items) {
+                    if (!executeValidations(item, c)) {
+                        isValid = false;
+                    }
+                }
+
                 continue;
             }
 
-            validators.forEach((cb) => cb(c, v));
+            if (!executeValidations(c[sectionId].items[itemId], c)) {
+                isValid = false;
+            }
+        }
+
+        return isValid;
+    }
+
+    for (const { items } of c) {
+        for (const item of items) {
+            if (!executeValidations(item, c)) {
+                isValid = false;
+            }
         }
     }
 
