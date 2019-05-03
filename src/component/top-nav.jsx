6c6fcfb6dbb6a4a192796a93ca5bc4a5385fc4bf
Eugene Matvejev <eugene.matvejev@gmail.com>
2019-05-03 15:03:06 +0100

    connect portal to /search

--- src/component/top-nav.jsx
+++ src/component/top-nav.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const TopNav = ({ className, 'data-cy': cy, initials }) =>
     <nav className={`topnav ${className}`}>
-        <button data-cy={`${cy}topnav-search`} className="topnav_search_button">search</button>
+        <Link
+            data-cy={`${cy}topnav-search`}
+            to="/search"
+            className="topnav_search_button"
+        >
+            search
+        </Link>
         <button data-cy={`${cy}topnav-profile`} className="topnav_profile_button">{initials}</button>
     </nav>;
 
