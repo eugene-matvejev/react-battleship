42d120ea58194ef89adffa0f7ca35067eebe3363
Eugene Matvejev <eugene.matvejev@gmail.com>
2019-06-26 15:37:42 +0100

    save progress

--- src/filtering/filter.test.js
+++ src/filtering/filter.test.js
@@ -33,29 +33,56 @@ describe('tree filter engine', () => {
         ['TEXT', 'te', [{ v: 'TE', isMatch: true }, { v: 'XT', isMatch: false }]],
     ];
 
-    describe(`detection of pattern in [::text] in lowercase mode`, () => {
-        dataProvider
-            .filter((v) => v[2])
-            .forEach(([text, pattern, c]) => {
-                describe(`searching for "${pattern}" in "${text}"`, () => {
-                    it(`expect [::chunks] to have length ${c.length}`, () => {
-                        const v = { text };
+    describe(`detection of pattern in root node`, () => {
+        [
+            [
+                'player',
+                'a',
+                [
+                    { v: 'pl', isMatch: false },
+                    { v: 'a', isMatch: true },
+                    { v: 'yer', isMatch: false },
+                ]
+            ],
+            ['text', 'text', [{ v: 'text', isMatch: true }]],
+            ['TEXT text', 'text', [{ v: 'TEXT', isMatch: true }, { v: ' ', isMatch: false }, { v: 'text', isMatch: true }]],
+            [
+                'TEXT text text text',
+                'text',
+                [
+                    { v: 'TEXT', isMatch: true },
+                    { v: ' ', isMatch: false },
+                    { v: 'text', isMatch: true },
+                    { v: ' ', isMatch: false },
+                    { v: 'text', isMatch: true },
+                    { v: ' ', isMatch: false },
+                    { v: 'text', isMatch: true },
+                ]
+            ],
+            ['TTT', 't', [{ v: 'T', isMatch: true }, { v: 'T', isMatch: true }, { v: 'T', isMatch: true }]],
+            ['T T', 't', [{ v: 'T', isMatch: true }, { v: ' ', isMatch: false }, { v: 'T', isMatch: true }]],
+            ['TEXT', 't', [{ v: 'T', isMatch: true }, { v: 'EX', isMatch: false }, { v: 'T', isMatch: true }]],
+            ['TEXT', 'te', [{ v: 'TE', isMatch: true }, { v: 'XT', isMatch: false }]],
+        ].forEach(([text, pattern, c]) => {
+            describe(`searching for "${pattern}" in "${text}"`, () => {
+                it(`expect [::chunks] to have length ${c.length}`, () => {
+                    const v = { text };
 
-                        filter(v, pattern);
-                        expect(v.chunks).toHaveLength(c.length);
-                    });
+                    filter(v, pattern);
+                    expect(v.chunks).toHaveLength(c.length);
+                });
 
-                    it(`expect [::isMatch] in chunk to be TRUE, if it matches ::pattern, otherwise FALSE`, () => {
-                        const v = { text };
+                it(`expect [::isMatch] in chunk to be TRUE, if it matches ::pattern, otherwise FALSE`, () => {
+                    const v = { text };
 
-                        filter(v, pattern);
-                        expect(v.chunks).toEqual(c);
-                    });
+                    filter(v, pattern);
+                    expect(v.chunks).toEqual(c);
                 });
             });
-
-        dataProvider
-            .filter((v) => !v[2])
+        });
+[
+    ['text', 'a', undefined],
+]
             .forEach(([text, pattern]) => {
                 it(`searching for "${pattern}" in "${text}", expect [::chunks] to be undefined`, () => {
                     const v = { text };
@@ -66,9 +93,10 @@ describe('tree filter engine', () => {
             });
     });
 
-    describe(`detection of pattern in lowercase mode in children nodes`, () => {
+    describe(`detection of pattern in children nodes & propogation of ::isExpanded, ::isVisible fields`, () => {
         [
             [
+                'aaa > bbb > CCC',
                 {
                     text: 'aaa',
                     nodes: [
@@ -112,9 +140,51 @@ describe('tree filter engine', () => {
                         },
                     ],
                 },
+            ],
+            [
+                'aaa > bbb > CCC',
+                {
+                    text: 'aaa',
+                    nodes: [
+                        {
+                            text: 'bbb',
+                            nodes: [
+                                {
+                                    text: 'CCC',
+                                    nodes: [
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+                '',
+                {
+                    text: 'aaa',
+                    isExpanded: false,
+                    isVisible: false,
+                    chunks: undefined,
+                    nodes: [
+                        {
+                            text: 'bbb',
+                            isExpanded: false,
+                            isVisible: false,
+                            chunks: undefined,
+                            nodes: [
+                                {
+                                    text: 'CCC',
+                                    isExpanded: false,
+                                    isVisible: false,
+                                    chunks: undefined,
+                                    nodes: [],
+                                },
+                            ],
+                        },
+                    ],
+                },
             ]
-        ].forEach(([v, pattern, expected]) => {
-            it('aaa > bbb > ccc', () => {
+        ].forEach(([title, v, pattern, expected]) => {
+            it(`${title} [pattern: "${pattern}"]`, () => {
                 filter(v, pattern);
 
                 expect(v).toEqual(expected);
@@ -122,27 +192,27 @@ describe('tree filter engine', () => {
         });
     });
 
-    describe('propogation of [::isExpanded] field', () => {
-        dataProvider.forEach(([text, pattern, c]) => {
-            it(`searching for "${pattern}" in "${text}", expect to be ${`${!!c}`.toUpperCase()}`, () => {
-                const v = { text };
+    // describe('propogation of [::isExpanded] field', () => {
+    //     dataProvider.forEach(([text, pattern, c]) => {
+    //         it(`searching for "${pattern}" in "${text}", expect to be ${`${!!c}`.toUpperCase()}`, () => {
+    //             const v = { text };
 
-                const result = filter(v, pattern);
+    //             const result = filter(v, pattern);
 
-                expect(result).toBe(!!c);
-            });
-        });
-    });
+    //             expect(result).toBe(!!c);
+    //         });
+    //     });
+    // });
 
-    describe('propogation of [::isVisible] field', () => {
-        dataProvider.forEach(([text, pattern, c]) => {
-            it(`searching for "${pattern}" in "${text}", expect to be ${`${!!c}`.toUpperCase()}`, () => {
-                const v = { text };
+    // describe('propogation of [::isVisible] field', () => {
+    //     dataProvider.forEach(([text, pattern, c]) => {
+    //         it(`searching for "${pattern}" in "${text}", expect to be ${`${!!c}`.toUpperCase()}`, () => {
+    //             const v = { text };
 
-                const result = filter(v, pattern);
+    //             const result = filter(v, pattern);
 
-                expect(result).toBe(!!c);
-            });
-        });
-    });
+    //             expect(result).toBe(!!c);
+    //         });
+    //     });
+    // });
 });
